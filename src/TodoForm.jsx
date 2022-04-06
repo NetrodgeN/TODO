b5039e5f3d3177a93c08e85b19afc4fda43d6edb
Yarolsav <netrodgen@rambler.ru>
2022-04-06 15:46:26 +0300

    Configure React app for deployment to GitHub Pages

---
+++ src/TodoForm.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+const TodoForm = ({addTask}) => {
+    const [userInput, setUserInput] = useState('')
+
+    const handleSubmit= (e) =>{
+        e.preventDefault()
+        addTask(userInput)
+        setUserInput('')
+    }
+    const handleChange =(e)=>{
+        setUserInput(e.currentTarget.value)
+    }
+    const handleKeyPress = (e) => {
+        if(e.key === 'Enter'){
+            handleSubmit(e)
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                value={userInput}
+                type="text"
+                onChange={handleChange}
+                onKeyDown={handleKeyPress}
+                placeholder="Введите значение..."
+            />
+            <button>Сохранить</button>
+        </form>
+    )
+};
+
+export default TodoForm;
